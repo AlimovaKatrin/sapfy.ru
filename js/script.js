ef97e7b4ae9215be3b4693e30e741d6739a56f87
Yuri Pliskovsky <yuri.pliskovsky@nevosoft.ru>
2020-07-10 20:29:21 +0300

    Added the use of CSS styles when scrolling, hover effects on links

--- js/script.js
+++ js/script.js
@@ -36,5 +36,53 @@ let sceneOne = document.getElementById('scene-one'),
     sceneTwo = document.getElementById('scene-two'),
     // parallaxOne = new Parallax(sceneOne, {        
     //     relativeInput: true}), 
-    parallaxOne = new Parallax(sceneOne), 
+    parallaxOne = new Parallax(sceneOne),
     parallaxTwo = new Parallax(sceneTwo);
+
+// Scroll
+let isScrolling = false;
+
+window.addEventListener("scroll", throttleScroll, false);
+
+function throttleScroll(e) {
+    if (isScrolling == false) {
+        window.requestAnimationFrame(function () {
+            scrolling(e);
+            isScrolling = false;
+        });
+    }
+    isScrolling = true;
+}
+
+document.addEventListener("DOMContentLoaded", scrolling, false);
+
+let scrollItems = document.querySelectorAll(".scroll");
+
+function scrolling(e) {
+    for (let i = 0; i < scrollItems.length; i++) {
+        let scrollItem = scrollItems[i];
+
+        if (isPartiallyVisible(scrollItem)) {
+            scrollItem.classList.add("vivify");
+            scrollItem.classList.add("pullUp");
+            scrollItem.classList.add("delay-300");
+        }
+    }
+}
+
+function isPartiallyVisible(el) {
+    let elementBoundary = el.getBoundingClientRect();
+    let top = elementBoundary.top;
+    let bottom = elementBoundary.bottom;
+    let height = elementBoundary.height;
+
+    return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+}
+
+function isFullyVisible(el) {
+    let elementBoundary = el.getBoundingClientRect();
+    let top = elementBoundary.top;
+    let bottom = elementBoundary.bottom;
+
+    return ((top >= 0) && (bottom <= window.innerHeight));
+}
