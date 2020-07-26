6c1084a6f6877a06808dd89ff8aa58262c8257b7
Yuri Pliskovsky <yuri.pliskovsky@nevosoft.ru>
2020-07-26 23:36:57 +0300

    Interface adaptation

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 // Font scaling
-let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-setHtmlSize();
+// let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+// setHtmlSize();
 
-function setHtmlSize() {
-    if (windowWidth < 1400 && windowWidth >= 320) {
-        document.getElementsByTagName('html')[0].style.fontSize = (windowWidth / 1400) * 10 + "px";
-    } else if (windowWidth >= 1400) {
-        document.getElementsByTagName('html')[0].style.fontSize = "10px";
-    } else if (windowWidth < 320) {
-        document.getElementsByTagName('html')[0].style.fontSize = (windowWidth / 320) * 10 + "px";
-    };
-};
-window.addEventListener('resize', function () {
-    windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body
-        .clientWidth;
-    setHtmlSize();
-});
-window.addEventListener("orientationchange", function () {
-    setTimeout(() => {
-        window.location.reload()
-    }, 500);
-});
+// function setHtmlSize() {
+//     if (windowWidth < 1400 && windowWidth >= 320) {
+//         document.getElementsByTagName('html')[0].style.fontSize = (windowWidth / 1400) * 10 + "px";
+//     } else if (windowWidth >= 1400) {
+//         document.getElementsByTagName('html')[0].style.fontSize = "10px";
+//     } else if (windowWidth < 320) {
+//         document.getElementsByTagName('html')[0].style.fontSize = (windowWidth / 320) * 10 + "px";
+//     };
+// };
+// window.addEventListener('resize', function () {
+//     windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body
+//         .clientWidth;
+//     setHtmlSize();
+// });
+// window.addEventListener("orientationchange", function () {
+//     setTimeout(() => {
+//         window.location.reload()
+//     }, 500);
+// });
 
 // "Note" button operation
 let note = document.querySelector('.menu__note'),
